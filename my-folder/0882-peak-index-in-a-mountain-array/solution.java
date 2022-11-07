054557bb8a8eb643827da8d604b76bf9a31c8a82
TomNguyen10 <dungnguyen_2025@depauw.edu>
2022-11-07 18:49:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 73 MB (7.22%)

--- my-folder/0882-peak-index-in-a-mountain-array/solution.java
+++ my-folder/0882-peak-index-in-a-mountain-array/solution.java
@@ -1,9 +1,9 @@
 class Solution {
-    public int peakIndexInMountainArray(int[] A) {
-        int l = 0, r = A.length - 1, m;
+    public int peakIndexInMountainArray(int[] arr) {
+        int l = 0, r = arr.length - 1, m;
         while (l < r) {
             m = (l + r) / 2;
-            if (A[m] < A[m + 1])
+            if (arr[m] < arr[m + 1])
                 l = m + 1;
             else
                 r = m;
