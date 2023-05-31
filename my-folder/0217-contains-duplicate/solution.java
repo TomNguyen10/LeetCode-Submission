e28fb124ef17aaa60d4781d50c8af1248492f0a1
TomNguyen10 <dungnguyen_2025@depauw.edu>
2023-05-31 14:26:35 +0000

    [LeetCode Sync] Runtime - 13 ms (41.91%), Memory - 54.9 MB (94.97%)

--- my-folder/0217-contains-duplicate/solution.java
+++ my-folder/0217-contains-duplicate/solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> distinct = new HashSet<Integer>();
-        for (int num : nums) {
-            if (distinct.contains(num)) {
-                return true;
-            }
-            distinct.add(num);
-        }
-        return false;
+       Set<Integer> theSet = new HashSet<Integer>();
+       for (int num : nums) {
+           if (theSet.contains(num)) {
+               return true;
+           }
+           theSet.add(num);
+       }
+       return false;
     }
 }
