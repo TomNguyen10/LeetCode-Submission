154e6ab5e458d40a84c7b1aeb1995320861c7bc5
TomNguyen10 <dungnguyen_2025@depauw.edu>
2023-06-05 08:38:56 +0000

    [LeetCode Sync] Runtime - 4 ms (87.42%), Memory - 55.7 MB (99.47%)

--- my-folder/0011-container-with-most-water/solution.java
+++ my-folder/0011-container-with-most-water/solution.java
@@ -3,14 +3,13 @@ class Solution {
         int i = 0;
         int j = height.length - 1;
         int water = 0;
+        
         while (i < j) {
             water = Math.max(water, Math.min(height[i], height[j]) * (j - i));
-            if (height[i] < height[j]) {
+            if (height[i] < height[j])
                 i++;
-            }
-            else {
+            else 
                 j--;
-            }
         }
         return water;
     }
