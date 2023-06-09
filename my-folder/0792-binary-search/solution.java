cbdc3d65d39f9af83bbdd0a9dd8ce71ec50ce737
TomNguyen10 <dungnguyen_2025@depauw.edu>
2023-06-09 10:19:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.4 MB (99.53%)

--- my-folder/0792-binary-search/solution.java
+++ my-folder/0792-binary-search/solution.java
@@ -1,16 +1,15 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
-        
-        while(low <= high){
-            int mid = (low + high) / 2;
-            if(nums[mid] == target) 
-                return mid;
-            else if(target > nums[mid]) 
-                low = mid + 1;
-            else 
-                high = mid - 1;
+        int i = 0;
+        int j = nums.length-1;
+        int mid;
+        while (i <= j) {
+            mid = (i+j)/2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) {
+                j = mid-1;
+            }
+            else i = mid+1;
         }
         return -1;
     }
