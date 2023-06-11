8fb5e01e4f89801851dd052cd212f906733a4e57
TomNguyen10 <dungnguyen_2025@depauw.edu>
2023-06-11 09:34:57 +0000

    [LeetCode Sync] Runtime - 12 ms (80.77%), Memory - 47.3 MB (82.99%)

--- my-folder/0347-top-k-frequent-elements/solution.java
+++ my-folder/0347-top-k-frequent-elements/solution.java
@@ -1,36 +1,32 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i : nums) {
-            if (!map.containsKey(i)) {
-                map.put(i, 1);
-            }
+            if (!map.containsKey(i)) map.put(i, 1);
             else {
                 int val = map.get(i);
                 map.put(i, val+1);
             }
         }
 
-        ArrayList<Integer>[] bucket = new ArrayList[nums.length + 1];
-        int[] res = new int[k];
-
-        for (Integer key : map.keySet()) {
+        ArrayList<Integer>[] bucket = new ArrayList[nums.length+1];
+        for (int key : map.keySet()) {
             int value = map.get(key);
-            if (bucket[value] == null) {
-                bucket[value] = new ArrayList<Integer>();
-            }
+            if (bucket[value] == null) bucket[value] = new ArrayList<Integer>();
             bucket[value].add(key);
         }
 
-        int idx = 0;
-
-        for (int i = nums.length; i >= 0; i--)
-            if (bucket[i] != null)
-                for (int val : bucket[i]){
-                    res[idx++] = val;
-                    if(idx == k)
-                        return res;
+        int[] res = new int[k];
+        int count = 0;
+        for (int i = nums.length; i >= 0; i--) {
+            if (bucket[i] != null) {
+                for (int r : bucket[i]) {
+                    res[count++] = r;
+                    if (count == k) return res;
                 }
+            }
+        }
+
         return res;
     }
 }
