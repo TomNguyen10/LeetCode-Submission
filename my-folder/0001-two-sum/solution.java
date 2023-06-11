edd90c1a4821194b2e4f808395e6da6e5a0f0323
TomNguyen10 <dungnguyen_2025@depauw.edu>
2023-06-11 09:00:41 +0000

    [LeetCode Sync] Runtime - 2 ms (97.73%), Memory - 43.7 MB (99.91%)

--- my-folder/0001-two-sum/solution.java
+++ my-folder/0001-two-sum/solution.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        Map<Integer,Integer> map = new HashMap<>();
-        
-        for(int i=0; i<nums.length; i++){
-            if(map.containsKey(target - nums[i])){
-                result[1] = i;
-                result[0] = map.get(target - nums[i]);
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                res[1] = i;
+                res[0] = map.get(target - nums[i]);
                 break;
             }
-            map.put(nums[i],i);
+            map.put(nums[i], i);
         }
-        
-        return result;
+        return res;
     }
 }
