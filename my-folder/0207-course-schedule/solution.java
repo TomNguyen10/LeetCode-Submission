1f330945918c1f0d3d2927a693215a6c43019a5b
TomNguyen10 <dungnguyen_2025@depauw.edu>
2023-07-17 07:15:51 +0000

    [LeetCode Sync] Runtime - 3 ms (93.90%), Memory - 43.7 MB (98.73%)

--- my-folder/0207-course-schedule/solution.java
+++ my-folder/0207-course-schedule/solution.java
@@ -1,38 +1,47 @@
-class Solution {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if (numCourses == 0 || prerequisites.length == 0) {
-            return true;
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
         }
 
-        int indegree[] = new int[numCourses];
-
-        for (int i = 0; i < prerequisites.length; i++) {
-            indegree[prerequisites[i][0]]++; 
+        for (int[] prerequisite : prerequisites) {
+            int course = prerequisite[0];
+            int prerequisiteCourse = prerequisite[1];
+            graph.get(course).add(prerequisiteCourse);
         }
 
-        Queue<Integer> queue = new LinkedList<Integer>();
+        boolean[] visited = new boolean[numCourses];
+        boolean[] dfsVisited = new boolean[numCourses];
 
-        for (int i = 0; i < numCourses; i++) 
-            if (indegree[i] == 0)
-            queue.add(i);
-
-        int canFinishCount = queue.size();  
-
-        while (!queue.isEmpty()) {
-            int prerequisite = queue.remove(); 
-            for (int i = 0; i < prerequisites.length; i++)  {
-
-                if (prerequisites[i][1] == prerequisite) { 
-                    indegree[prerequisites[i][0]]--;
-                    
-                    if (indegree[prerequisites[i][0]] == 0) {
-                        canFinishCount++;
-                        queue.add(prerequisites[i][0]);
-                    }
-                }
+        for (int i = 0; i < numCourses; i++) {
+            if (!visited[i] && hasCycle(graph, visited, dfsVisited, i)) {
+                return false;
             }
         }
 
-        return (canFinishCount == numCourses);    
+        return true;
+    }
+
+    private boolean hasCycle(List<List<Integer>> graph, boolean[] visited, boolean[] dfsVisited, int course) {
+        visited[course] = true;
+        dfsVisited[course] = true;
+
+        for (int prerequisiteCourse : graph.get(course)) {
+            if (!visited[prerequisiteCourse]) {
+                if (hasCycle(graph, visited, dfsVisited, prerequisiteCourse)) {
+                    return true;
+                }
+            } else if (dfsVisited[prerequisiteCourse]) {
+                return true;
+            }
+        }
+
+        dfsVisited[course] = false;
+        return false;
     }
 }
+
