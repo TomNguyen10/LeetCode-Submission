96a80dde53e21ebbc34dd71a751d82d74537a66d
TomNguyen10 <dungnguyen_2025@depauw.edu>
2023-07-17 04:46:50 +0000

    [LeetCode Sync] Runtime - 18 ms (20.10%), Memory - 41.8 MB (98.47%)

--- my-folder/0205-isomorphic-strings/solution.java
+++ my-folder/0205-isomorphic-strings/solution.java
@@ -1,23 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s == null || s.length() <= 1) return true;
-        HashMap<Character, Character> map = new HashMap<Character, Character>();
-        for(int i = 0 ; i< s.length(); i++){
-            char a = s.charAt(i);
-            char b = t.charAt(i);
-            if(map.containsKey(a)){
-                 if(map.get(a).equals(b))
-                continue;
-                else
-                return false;
-            }else{
-                if(!map.containsValue(b))
-                map.put(a,b);
-                else return false;
-                
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Character> mapS = new HashMap<>();
+        Map<Character, Character> mapT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+
+            if (mapS.containsKey(charS)) {
+                if (mapS.get(charS) != charT) {
+                    return false;
+                }
+            } else {
+                mapS.put(charS, charT);
+            }
+
+            if (mapT.containsKey(charT)) {
+                if (mapT.get(charT) != charS) {
+                    return false;
+                }
+            } else {
+                mapT.put(charT, charS);
             }
         }
+
         return true;
-        
     }
 }
+
