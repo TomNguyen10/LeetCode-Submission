03e683d8a98806280f27c9e1cb57f8e441e08673
TomNguyen10 <dungnguyen_2025@depauw.edu>
2023-08-24 14:32:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.1 MB (99.96%)

--- my-folder/0904-leaf-similar-trees/solution.java
+++ my-folder/0904-leaf-similar-trees/solution.java
@@ -17,20 +17,19 @@ class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
-        
-        dfs(list1, root1);
-        dfs(list2, root2);
-        
+        dfs(root1, list1);
+        dfs(root2, list2);
+
         return list1.equals(list2);
     }
- 
-    private void dfs (List<Integer> list, TreeNode root) {
+
+    private void dfs(TreeNode root, List<Integer> list) {
         if (root == null) return;
         if (root.left == null && root.right == null) {
             list.add(root.val);
         }
-        dfs(list, root.left);
-        dfs(list, root.right);
+        dfs(root.left, list);
+        dfs(root.right, list);
     }
 }
 
