081537c9c7f813ccde39191eb026e3e0a587fc28
TomNguyen10 <dungnguyen_2025@depauw.edu>
2023-09-27 00:53:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.1 MB (99.96%)

--- my-folder/0871-keys-and-rooms/solution.java
+++ my-folder/0871-keys-and-rooms/solution.java
@@ -1,23 +1,20 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         boolean[] visited = new boolean[rooms.size()];
-        Stack<Integer> stack = new Stack<>();
         visited[0] = true;
-        stack.push(0);
-        while (!stack.isEmpty()) {
-            int room = stack.pop();
-            for (int key : rooms.get(room)) {
-                if (!visited[key]) {
-                    visited[key] = true;
-                    stack.push(key);
-                }
+        dfs(rooms, 0, visited);
+        for (boolean visit : visited) {
+            if (visit == false) return false;
+        }
+        return true;
+    }
+
+    private void dfs(List<List<Integer>> rooms, int room, boolean[] visited) {
+        for (int key : rooms.get(room)) {
+            if (!visited[key]) {
+                visited[key] = true;
+                dfs(rooms, key, visited);
             }
         }
-
-        for (boolean visits : visited) {
-            if (visits == false) return false;
-        }
-
-        return true;
     }
 }
