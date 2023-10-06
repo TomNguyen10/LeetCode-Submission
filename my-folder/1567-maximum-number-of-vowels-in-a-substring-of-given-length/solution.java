d4d18a2a31843a581ef100f9fce5c7ad71363181
TomNguyen10 <dungnguyen_2025@depauw.edu>
2023-10-06 03:48:36 +0000

    [LeetCode Sync] Runtime - 23 ms (29.40%), Memory - 43.8 MB (99.03%)

--- my-folder/1567-maximum-number-of-vowels-in-a-substring-of-given-length/solution.java
+++ my-folder/1567-maximum-number-of-vowels-in-a-substring-of-given-length/solution.java
@@ -1,36 +1,31 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        int maxVowels = 0;
-        int currentVowels = 0;
-        Set<Character> vowels = new HashSet<>();
-        vowels.add('a');
-        vowels.add('e');
-        vowels.add('i');
-        vowels.add('o');
-        vowels.add('u');
-
+        int max = 0;
+        int cur = 0;
+        Set<Character> set = new HashSet<>();
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
         for (int i = 0; i < k; i++) {
-            if (vowels.contains(s.charAt(i))) {
-                currentVowels++;
+            char c = s.charAt(i);
+            if (set.contains(c)) {
+                cur++;
             }
         }
-        maxVowels = Math.max(maxVowels, currentVowels);
-
+        max = Math.max(max, cur);
         for (int i = k; i < s.length(); i++) {
-            char leftChar = s.charAt(i - k); 
-            char rightChar = s.charAt(i);    
-
-            if (vowels.contains(leftChar)) {
-                currentVowels--;
+            char left = s.charAt(i - k);
+            char right = s.charAt(i);
+            if (set.contains(left)) {
+                cur--;
             }
-            if (vowels.contains(rightChar)) {
-                currentVowels++;
+            if (set.contains(right)) {
+                cur++;
             }
-
-            maxVowels = Math.max(maxVowels, currentVowels);
+            max = Math.max(max, cur);
         }
-
-        return maxVowels;
-
+        return max;
     }
 }
