b1922045f8c159f4e41cec9368c0b44bc0e4d666
TomNguyen10 <dungnguyen_2025@depauw.edu>
2023-10-10 18:50:36 +0000

    [LeetCode Sync] Runtime - 3 ms (31.56%), Memory - 43.3 MB (12.73%)

--- my-folder/2076-sum-of-digits-of-string-after-convert/solution.java
+++ my-folder/2076-sum-of-digits-of-string-after-convert/solution.java
@@ -1,26 +1,19 @@
 class Solution {
     public int getLucky(String s, int k) {
-        int[] lettersTransformedOnce  = new int[26];
-        for(int i = 0; i< 26;i++ ){
-            int curLetterNum = i +1;
-            while(curLetterNum > 0) {
-                lettersTransformedOnce[i] += curLetterNum%10; 
-                curLetterNum /= 10;
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            int i = c - 'a' + 1;
+            String add = String.valueOf(i);
+            sb.append(add);
+        }
+        String news = sb.toString();
+        for (int i = 0; i < k; i++) {
+            int total = 0;
+            for (char c : news.toCharArray()) {
+                total += Integer.parseInt(String.valueOf(c));
             }
+            news = String.valueOf(total);
         }
-        int result = 0;
-        for(int i = 0 ; i < s.length() ; i++ ) {
-            int letterIdx = s.charAt(i) - 'a';
-            result += lettersTransformedOnce[letterIdx]; 
-        }
-        for(int i=1;i<k;i++ ) {
-            int temp = result;
-            result =0;
-            while(temp>0) {
-                result += temp% 10;
-                temp/=10;
-            }
-        }
-        return result;
+        return Integer.parseInt(String.valueOf(news));
     }
 }
