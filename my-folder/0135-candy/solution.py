56715d093966a0dfda654f88991324f1cb5dcc58
TomNguyen10 <dungnguyen_2025@depauw.edu>
2023-10-24 18:39:07 +0000

    [LeetCode Sync] Runtime - 142 ms (11.86%), Memory - 19.1 MB (95.00%)

---
+++ my-folder/0135-candy/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        candy = [1] * n
+
+        for i in range(1, n):
+            if ratings[i-1] < ratings[i]:
+                candy[i] = candy[i-1] + 1
+
+        for i in range(n-1, 0, -1):
+            if ratings[i-1] > ratings[i]:
+                candy[i-1] = max(candy[i]+1, candy[i-1])
+        
+        res = sum(candy)
+        return res
+        
+        
