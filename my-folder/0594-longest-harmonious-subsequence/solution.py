535345308befac7869f53c4c8af7579297e65184
TomNguyen10 <dungnguyen_2025@depauw.edu>
2023-11-28 20:39:15 +0000

    [LeetCode Sync] Runtime - 251 ms (44.92%), Memory - 18.4 MB (43.33%)

---
+++ my-folder/0594-longest-harmonious-subsequence/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        m = defaultdict(int)
+        for num in nums:
+            m[num] += 1
+        res = 0
+        for key in m:
+            if key + 1 in m:
+                res = max(res, m[key] + m[key+1])
+        return res
+        
+
+
