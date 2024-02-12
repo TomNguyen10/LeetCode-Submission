c5938e953af385c3148ba62ac831f399e5a7e0a0
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-02-12 00:21:47 +0000

    [LeetCode Sync] Runtime - 135 ms (99.80%), Memory - 18.1 MB (26.70%)

--- my-folder/0169-majority-element/solution.py
+++ my-folder/0169-majority-element/solution.py
@@ -1,14 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count, candidate = 0, 0
-
+        count, candidate = 0, nums[0]
         for num in nums:
-            if (count == 0):
+            if count == 0:
                 candidate = num
-            if (num == candidate):
+            if num == candidate:
                 count += 1
             else:
-                count -= 1
-        
+                count -=1
         return candidate
-        
