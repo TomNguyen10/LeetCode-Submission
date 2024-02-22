396277ab2abbd1ecccb8f29051c8679038cea237
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-02-22 05:26:19 +0000

    [LeetCode Sync] Runtime - 50 ms (65.75%), Memory - 16.6 MB (97.89%)

---
+++ my-folder/1045-check-if-word-is-valid-after-substitutions/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for char in s:
+            if char == 'c':
+                if not stack or stack.pop() != 'b':
+                    return False
+                if not stack or stack.pop() != 'a':
+                    return False
+            else:
+                stack.append(char)
+        
+        return not stack
