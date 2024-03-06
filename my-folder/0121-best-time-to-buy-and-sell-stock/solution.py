efd1e8c535b07dc8cdb4ccf011cf51c35c216c7d
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-03-06 04:01:18 +0000

    [LeetCode Sync] Runtime - 778 ms (25.74%), Memory - 27.4 MB (28.12%)

--- my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -5,8 +5,9 @@ class Solution:
         
         maxProfit = 0
         minPurchase = prices[0]
-        for i in range (1, len(prices)):
-            maxProfit = max(maxProfit, prices[i] - minPurchase)
+        for i in range(1, len(prices)):
+            maxProfit = max(maxProfit, prices[i]-minPurchase)
             minPurchase = min(minPurchase, prices[i])
+
         return maxProfit
-        
+
