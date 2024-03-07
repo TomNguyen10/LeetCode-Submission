ab1e476e266e3ea08849cdee4dcb14176f3e8040
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-03-07 00:44:56 +0000

    [LeetCode Sync] Runtime - 280 ms (49.83%), Memory - 63.7 MB (59.56%)

--- my-folder/0380-insert-delete-getrandom-o1/solution.py
+++ my-folder/0380-insert-delete-getrandom-o1/solution.py
@@ -1,8 +1,8 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.hash_map = {}
         self.arr = []
+        self.hash_map = {}
 
     def insert(self, val: int) -> bool:
         if val not in self.hash_map:
@@ -17,14 +17,14 @@ class RandomizedSet:
             if location < len(self.arr) - 1:
                 last = self.arr[-1]
                 self.hash_map[last], self.arr[location] = location, last
-            self.hash_map.pop(val, 0)
             self.arr.pop()
+            self.hash_map.pop(val)
             return True
         return False
 
     def getRandom(self) -> int:
-        rand = random.randint(0, len(self.arr) - 1)
-        return self.arr[rand]
+        loc = random.randint(0, len(self.arr) - 1)
+        return self.arr[loc]
 
 
 # Your RandomizedSet object will be instantiated and called as such:
