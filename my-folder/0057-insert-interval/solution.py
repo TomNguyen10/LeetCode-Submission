91ac035d725a6f7fa005b9bb57310584c3ccff59
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-03-10 19:35:40 +0000

    [LeetCode Sync] Runtime - 69 ms (86.65%), Memory - 19.8 MB (68.85%)

--- my-folder/0057-insert-interval/solution.py
+++ my-folder/0057-insert-interval/solution.py
@@ -1,27 +1,24 @@
-from typing import List
-
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        if not intervals:
-            return [newInterval]
+        if not intervals: return [newInterval]
 
-        result = []
+        res = []
+
         inserted = False
 
         for interval in intervals:
             if interval[1] < newInterval[0]:
-                result.append(interval)
+                res.append(interval)
             elif interval[0] > newInterval[1]:
                 if not inserted:
-                    result.append(newInterval)
+                    res.append(newInterval)
                     inserted = True
-                result.append(interval)
+                res.append(interval)
             else:
                 newInterval[0] = min(newInterval[0], interval[0])
                 newInterval[1] = max(newInterval[1], interval[1])
-
+        
         if not inserted:
-            result.append(newInterval)
+            res.append(newInterval)
 
-        return result
-
+        return res
