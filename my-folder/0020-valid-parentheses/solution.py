21301d6b19812e4dde7df1c5bba3e5399dcfc610
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-03-10 04:12:10 +0000

    [LeetCode Sync] Runtime - 38 ms (38.52%), Memory - 16.6 MB (63.15%)

---
+++ my-folder/0020-valid-parentheses/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for char in s:
+            if char == '(' or char == '[' or char == '{':
+                stack.append(char)
+            else:
+                if not stack: return False
+                top = stack[-1]
+                if (char == ')' and top == '(') or (char == ']' and top == '[') or (char == '}' and top == '{'):
+                    stack.pop()
+                else:
+                    stack.append(char)
+
+        return not stack
