c6705ff2919dda9fbef6a0e01ecc39fdbb62d9bc
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-03-11 20:34:30 +0000

    [LeetCode Sync] Runtime - 39 ms (53.00%), Memory - 16.6 MB (51.14%)

--- my-folder/3055-maximum-odd-binary-number/solution.py
+++ my-folder/3055-maximum-odd-binary-number/solution.py
@@ -1,7 +1,9 @@
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
-        n = len(s)
         ones = s.count('1')
-        zeros = n - ones
-        result = '1' * (ones - 1) + '0' * zeros + '1'
-        return result
+
+        zeros = len(s) - ones
+
+        res = '1' * (ones - 1) + '0' * zeros + '1'
+
+        return res
