bac6f6c2a504b1db6e97793a45afe0b78569873b
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-03-14 04:05:45 +0000

    [LeetCode Sync] Runtime - 225 ms (44.46%), Memory - 20.5 MB (11.03%)

---
+++ my-folder/0966-binary-subarrays-with-sum/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        dic = {0: 1}
+        cum = 0
+        res = 0
+
+        for num in nums:
+            cum += num
+            if cum - goal in dic:
+                res += dic[cum-goal]
+            dic[cum] = dic.get(cum, 0) + 1
+        
+        return res
