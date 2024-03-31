aa7e849d46c16b79f6dab8a37a3e6ed1d9a81bd1
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-03-31 17:42:15 +0000

    [LeetCode Sync] Runtime - 288 ms (99.23%), Memory - 30.4 MB (49.11%)

--- my-folder/0041-first-missing-positive/solution.py
+++ my-folder/0041-first-missing-positive/solution.py
@@ -7,8 +7,8 @@ class Solution:
             if nums[i] < 0 or nums[i] >= n:
                 nums[i] = 0
         
-        for num in nums:
-            nums[num % n] += n
+        for i in range(n):
+            nums[nums[i] % n] += n
         
         for i in range(1, n):
             if nums[i] // n == 0: return i
