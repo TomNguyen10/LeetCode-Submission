55b4e0b0b88ee5e7bb2f204a1fc0d470ef307987
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-01 00:05:51 +0000

    [LeetCode Sync] Runtime - 30 ms (84.88%), Memory - 16.7 MB (22.53%)

--- my-folder/0058-length-of-last-word/solution.py
+++ my-folder/0058-length-of-last-word/solution.py
@@ -1,5 +1,6 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+        if not s: return 0
         s = s.strip()
         arr = s.split(" ")
-        return len(arr[-1])        
+        return len(arr[-1]) 
