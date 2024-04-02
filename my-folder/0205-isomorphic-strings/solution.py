817bf07b2c61ddc0d3367dc590bf0fc5f6d82bf4
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-02 00:26:14 +0000

    [LeetCode Sync] Runtime - 51 ms (12.86%), Memory - 16.8 MB (51.64%)

--- my-folder/0205-isomorphic-strings/solution.py
+++ my-folder/0205-isomorphic-strings/solution.py
@@ -1,20 +1,18 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+
         map1, map2 = {}, {}
 
         for i in range(len(s)):
             c1, c2 = s[i], t[i]
+            if c1 in map1 and map1[c1] != c2:
+                return False
+            
+            if c2 in map2 and map2[c2] != c1:
+                return False
+            
+            map1[c1] = c2
+            map2[c2] = c1
 
-            if c1 in map1:
-                if map1[c1] != c2:
-                    return False
-            else:
-                map1[c1] = c2
-
-            if c2 in map2:
-                if map2[c2] != c1:
-                    return False
-            else:
-                map2[c2] = c1
-
-        return True 
+        return True
