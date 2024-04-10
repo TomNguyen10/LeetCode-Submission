7d4dbfbf190fc50b98302242bcd326b0dd60ab27
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-10 04:37:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (79.28%)

--- my-folder/0021-merge-two-sorted-lists/solution.java
+++ my-folder/0021-merge-two-sorted-lists/solution.java
@@ -10,16 +10,30 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null) return list2;
-        if (list2 == null) return list1;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
 
-        if (list1.val < list2.val) {
-            list1.next = mergeTwoLists(list1.next, list2);
-            return list1;
-        } 
-        else {
-            list2.next = mergeTwoLists(list2.next, list1);
-            return list2;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                curr = curr.next;
+                list1 = list1.next;
+            }
+            else {
+                curr.next = list2;
+                curr = curr.next;
+                list2 = list2.next;
+            }
         }
+
+        if (list1 != null) {
+            curr.next = list1;
+        }
+
+        if (list2 != null) {
+            curr.next = list2;
+        }
+
+        return dummy.next;
     }
 }
