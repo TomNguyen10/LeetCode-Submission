6b2b8af1ab8828d4819df7543bc9be0cc01d936b
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-10 17:47:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.3 MB (99.05%)

--- my-folder/0226-invert-binary-tree/solution.java
+++ my-folder/0226-invert-binary-tree/solution.java
@@ -15,10 +15,15 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if (root == null) return null;
+        if (root == null) {
+            return null;
+        }
+
         TreeNode temp = new TreeNode(root.val);
+
         temp.left = invertTree(root.right);
         temp.right = invertTree(root.left);
+
         return temp;
     }
 }
