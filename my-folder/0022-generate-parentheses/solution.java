7166b8c6b8de8494d0ba81984d907a84c5fe3af5
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-11 21:06:42 +0000

    [LeetCode Sync] Runtime - 1 ms (83.57%), Memory - 43.1 MB (55.28%)

--- my-folder/0022-generate-parentheses/solution.java
+++ my-folder/0022-generate-parentheses/solution.java
@@ -1,17 +1,22 @@
 class Solution {
-    public ArrayList<String> generateParenthesis(int n) {
-        ArrayList<String> res = new ArrayList<String>();
-        backtrack(res, "", 0, 0, n);
-        return res;
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", n, n);
+        return result;
     }
-    
-    public void backtrack(List<String> list, String input, int open, int close, int max){
-        if (input.length() == max*2) {
-            list.add(input);
+
+    public void backtrack(List<String> list, String current, int open, int close) {
+        if (open == 0 && close == 0) {
+            list.add(current);
             return;
         }
-        
-        if (open < max) backtrack (list, input + "(", open+1, close,max);
-        if (close < open) backtrack (list, input + ")", open, close+1, max);   
+
+        if (open > 0) {
+            backtrack(list, current + '(', open-1, close);
+        }
+        if (close > open) {
+            backtrack(list, current + ')', open, close -1);
+        }
     }
+
 }
