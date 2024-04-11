e83304807195b05fecb72e3e0bab8ce4628b7d0a
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-11 00:47:24 +0000

    [LeetCode Sync] Runtime - 23 ms (54.21%), Memory - 44.8 MB (82.24%)

---
+++ my-folder/0402-remove-k-digits/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        Stack<Character> stack = new Stack<>();
+        for (char letter : num.toCharArray()) {
+            while (!stack.isEmpty() && stack.peek() > letter && k > 0) {
+                stack.pop();
+                k--;
+            }
+            stack.push(letter);
+        }
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+        ArrayList<Character> res = new ArrayList<>(stack);
+        while (!res.isEmpty() && res.get(0) == '0') {
+            res.remove(0);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char c : res) {
+            sb.append(c);
+        }
+        String ans = sb.toString();
+        if (ans.equals("")) {
+            return "0";
+        }
+        return ans;
+    }
+}
