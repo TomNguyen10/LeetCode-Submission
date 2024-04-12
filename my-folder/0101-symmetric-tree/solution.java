0ecf62c93b3e9eddeedf5a38d9dce551c1ecd2b2
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-12 04:17:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.5 MB (73.35%)

--- my-folder/0101-symmetric-tree/solution.java
+++ my-folder/0101-symmetric-tree/solution.java
@@ -14,17 +14,20 @@
  * }
  */
 class Solution {
-   public boolean isSymmetric(TreeNode root) {
-       if (root == null)
-           return true;
-       return isSymmetricHelp(root.left, root.right);
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return bfs(root.left, root.right);
     }
 
-    private boolean isSymmetricHelp(TreeNode left, TreeNode right){
-        if(left==null || right==null)
-            return left==right;
-        if(left.val!=right.val)
+    private boolean bfs(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || left.val != right.val) {
             return false;
-        return isSymmetricHelp(left.left, right.right) && isSymmetricHelp(left.right,         right.left);
+        }
+        return bfs(left.left, right.right) && bfs(left.right, right.left);
     }
 }
