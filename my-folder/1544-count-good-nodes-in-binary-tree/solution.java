6cfea734b4293ecc42db83323bcb6ad6ea057acd
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-13 00:26:58 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 52.6 MB (19.22%)

--- my-folder/1544-count-good-nodes-in-binary-tree/solution.java
+++ my-folder/1544-count-good-nodes-in-binary-tree/solution.java
@@ -18,17 +18,18 @@ class Solution {
         return dfs(root, root.val);
     }
 
-    private int dfs (TreeNode root, int max) {
-        if (root == null) return 0;
+    private int dfs(TreeNode root, int max) {
+        if (root == null) {
+            return 0;
+        }
         int res = 0;
-        if (root.val >= max ) {
+        if (root.val >= max) {
             res = 1;
             max = root.val;
         }
-
         res += dfs(root.left, max);
         res += dfs(root.right, max);
-
+        
         return res;
     }
 }
