552174a1fb4826d9c82e833a358e00ba7a6804a3
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-17 03:02:55 +0000

    [LeetCode Sync] Runtime - 75 ms (33.51%), Memory - 62.5 MB (43.21%)

--- my-folder/0739-daily-temperatures/solution.java
+++ my-folder/0739-daily-temperatures/solution.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        int[] res = new int[temperatures.length];
         Stack<Integer> stack = new Stack<>();
-        int prev;
-        for (int i = 0; i < res.length; i++) {
-            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
-                prev = stack.pop();
-                res[prev] = i - prev;
+        int[] res = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            if (stack.isEmpty()) {
+                stack.push(i);
             }
-            stack.push(i);
+            else {
+                while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                    res[stack.peek()] = i - stack.pop();
+                }
+                stack.push(i);
+            }
         }
         return res;
     }
