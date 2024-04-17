bd883c02b1cc7629834867fabedcc4f9834f2146
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-17 20:25:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.2 MB (15.80%)

--- my-folder/0153-find-minimum-in-rotated-sorted-array/solution.java
+++ my-folder/0153-find-minimum-in-rotated-sorted-array/solution.java
@@ -1,21 +1,21 @@
 class Solution {
     public int findMin(int[] nums) {
+        // Implement the binary search technique
+        // Calculate the midde, if the number in the middle is larger 
+        // than the number at the end side, it means that the minimum 
+        // is on the right side. Update the left to be the middle + 1
+        // Else the minimum is on the left side update the right side to be middle
         int left = 0;
         int right = nums.length - 1;
-        while (left <= right) {
-            if (nums[left] <= nums[right]) {
-                return nums[left];
-            }
-            
-            int mid = (right + left) / 2;
-            if (nums[mid] >= nums[left]) {
-                left = mid + 1;
+        while (left < right) {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] > nums[right]) {
+                left = middle + 1;
             }
             else {
-                right = mid;
+                right = middle;
             }
         }
-
-        return nums[right];
+        return nums[left];
     }
 }
