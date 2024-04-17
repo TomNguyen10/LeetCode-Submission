9c059fb0262795e264216f1108c81ab742067a90
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-17 22:28:26 +0000

    [LeetCode Sync] Runtime - 145 ms (56.21%), Memory - 120 MB (38.98%)

--- my-folder/1023-time-based-key-value-store/solution.java
+++ my-folder/1023-time-based-key-value-store/solution.java
@@ -1,30 +1,38 @@
 class TimeMap {
 
-    private HashMap<String, ArrayList<Pair<String, Integer>>> map;
+    Map<String, List<Pair<String, Integer>>> map;
 
     public TimeMap() {
         map = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
-        if (!map.containsKey(key)) map.put(key, new ArrayList<>());
+        if (!map.containsKey(key)) {
+            map.put(key, new ArrayList<>());
+        }
         map.get(key).add(new Pair(value, timestamp));
     }
     
     public String get(String key, int timestamp) {
-        if (!map.containsKey(key)) return "";
-        ArrayList<Pair<String, Integer>> list = map.get(key);
+        if (!map.containsKey(key)) {
+            return "";
+        }
+        List<Pair<String, Integer>> list = map.get(key);
         int left = 0;
         int right = list.size() - 1;
         while (left < right) {
             int mid = left + (right - left + 1) / 2;
-            if (list.get(mid).getValue() <= timestamp) left = mid; else right = mid - 1;
+            if (list.get(mid).getValue() <= timestamp) {
+                left = mid;
+            }
+            else {
+                right = mid - 1;
+            }
         }
-        
-        return list.get(left).getValue() <= timestamp
-            ? list.get(left).getKey()
-            : "";
-
+        if (list.get(left).getValue() > timestamp) {
+            return "";
+        }
+        return list.get(left).getKey();
     }
 }
 
