14cfceca0db9651738979220b4fafe80afc9e394
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-17 20:57:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.9 MB (42.13%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.java
+++ my-folder/0033-search-in-rotated-sorted-array/solution.java
@@ -1,27 +1,39 @@
 class Solution {
     public int search(int[] nums, int target) {
-
-        int l = 0;
-        int r = nums.length - 1;
-
-        while(l<=r){
-
-            int mid = (l+r)/2;
-
-            if(nums[mid] == target) return mid;
-
-            if(nums[l]<=nums[mid]){
-                if(target > nums[mid] || target < nums[l])l = mid + 1;
-                else r = mid - 1;
-        
-            }else{
-                if(target < nums[mid] || target > nums [r]) r = mid - 1;
-                else l = mid + 1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
             }
-
-        }
-
+            //Check if the left half is sorted
+            if (nums[left] <= nums[mid]) {
+                //If the left half is sorted:
+                //If the target is within the sorted left half,
+                //adjust the right pointer to search the left half.
+                //Otherwise, adjust the left pointer to search the right half.
+                if (target > nums[mid] || target < nums[left]) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+            //If the right half is sorted
+            else {
+                //If the right half is sorted:
+                //If the target is within the sorted right half,
+                //adjust the left pointer to search the right half.
+                //Otherwise, adjust the right pointer to search the left half.
+                if (target < nums[mid] || target > nums[right]) {
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+        } 
         return -1;
     }
 }
-
