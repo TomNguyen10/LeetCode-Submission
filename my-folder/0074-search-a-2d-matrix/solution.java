b2c576640456579182529f67af8da3c39f10da1a
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-17 21:21:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.3 MB (20.43%)

--- my-folder/0074-search-a-2d-matrix/solution.java
+++ my-folder/0074-search-a-2d-matrix/solution.java
@@ -1,12 +1,18 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int i = 0;
-        int j = matrix[0].length-1;
-        while (i < matrix.length && j >= 0) {
-            if (matrix[i][j] == target) return true;
-            else if (matrix[i][j] > target) j--;
-            else i++;
+        int r = matrix.length;
+        int c = matrix[0].length;
+        int left = 0;
+        int right = r*c - 1;
+        while (left != right) {
+            int mid = (left + right) / 2;
+            if (matrix[mid/c][mid%c] < target) {
+                left = mid+1;
+            }
+            else {
+                right = mid;
+            }
         }
-        return false;
+        return matrix[right/c][right%c] == target;
     }
 }
