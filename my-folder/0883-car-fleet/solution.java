999950f54a13566bf0bab2d0e63054a6141772d8
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-17 04:24:04 +0000

    [LeetCode Sync] Runtime - 20 ms (85.96%), Memory - 56.7 MB (76.75%)

--- my-folder/0883-car-fleet/solution.java
+++ my-folder/0883-car-fleet/solution.java
@@ -1,24 +1,18 @@
 class Solution {
-    public int carFleet(int target, int[] position, int[] speed) {//tc = O(nlogn) sc = O(n)
-        if (position.length == 1 || position.length == 0) return position.length;
-        
-        // number of fleet:
-        int numberOfCarFleet = position.length;
-
-        float [] arraytime = new float[target];
-
-        for (int i = 0; i < position.length; i++) {
-            arraytime[position[i]] = (float)(target - position[i]) / speed[i];
+    public int carFleet(int target, int[] position, int[] speed) {
+        if (position.length == 1 || position.length == 0) {
+            return position.length;
         }
-
-        float previousTime = 0;
+        double [] times = new double[target];
+        for (int i = 0; i < position.length; i++) {
+            times[position[i]] = (double) (target - position[i]) / speed[i];
+        }
+        double prev = 0;
         int res = 0;
-
-        for (int i = target-1; i >=0 ; i--) {
-            float currentTime = arraytime[i];
-
-            if (currentTime > previousTime) {
-                previousTime = currentTime;
+        for (int i = target - 1; i >=0; i--) {
+            double current = times[i];
+            if (current > prev) {
+                prev = current;
                 res++;
             }
         }
