57ac9203751239d26178231dfe7ad42ceab33a4f
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-18 02:02:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.1 MB (86.51%)

--- my-folder/0138-copy-list-with-random-pointer/solution.java
+++ my-folder/0138-copy-list-with-random-pointer/solution.java
@@ -15,26 +15,21 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        if (head == null) return head;
-
-        Node temp = head;
-
-        HashMap<Node, Node> map = new HashMap<>();
-
-        while (temp != null) {
-            map.put(temp, new Node(temp.val));
-            temp = temp.next;
+        if (head == null) {
+            return null;
         }
-
-        temp = head;
-
-        while (temp != null) {
-            map.get(temp).next = map.get(temp.next);
-            map.get(temp).random = map.get(temp.random);
-            temp = temp.next;
+        Map<Node, Node> map = new HashMap<>();
+        Node curr = head;
+        while (curr != null) {
+            map.put(curr, new Node(curr.val));
+            curr = curr.next;
+        } 
+        curr = head;
+        while (curr != null) {
+            map.get(curr).next = map.get(curr.next);
+            map.get(curr).random = map.get(curr.random);
+            curr = curr.next;
         }
-
         return map.get(head);
-
     }
 }
