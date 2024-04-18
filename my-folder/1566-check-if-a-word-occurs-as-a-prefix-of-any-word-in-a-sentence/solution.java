8092467b45a0f2b519bf7bcc7e10a7b5417b2015
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-18 02:34:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.9 MB (7.18%)

--- my-folder/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/solution.java
+++ my-folder/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/solution.java
@@ -1,9 +1,11 @@
 class Solution {
     public int isPrefixOfWord(String sentence, String searchWord) {
-        String[] words = sentence.split(" ");
-        for (int i = 1; i <= words.length; ++i) {
-            if (words[i - 1].startsWith(searchWord)) {
-                return i;
+        sentence = sentence.strip();
+        String[] arr = sentence.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            String s = arr[i];
+            if (s.startsWith(searchWord)) {
+                return i+1;
             }
         }
         return -1;
