ae7371316afc38c684e7ce0daf54d7451724528b
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-18 02:25:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.3 MB (12.74%)

--- my-folder/0543-diameter-of-binary-tree/solution.java
+++ my-folder/0543-diameter-of-binary-tree/solution.java
@@ -17,20 +17,19 @@ class Solution {
 
     int diameter = 0;
 
-    public int dfs(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-
-        int left = dfs(root.left);
-        int right = dfs(root.right);
-        this.diameter = Math.max(diameter, (left + right));
-        return 1 + Math.max(left, right);
-    }
-
     public int diameterOfBinaryTree(TreeNode root) {    
         dfs(root);
         return this.diameter;
     }   
+
+    int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        this.diameter = Math.max(diameter, left + right);
+        return 1 + Math.max(left, right);
+    }
 
 }
