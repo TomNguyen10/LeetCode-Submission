ad1aaa64cfb3d34ca9fa9bd35a6c0b2065ece76e
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-18 00:10:45 +0000

    [LeetCode Sync] Runtime - 11 ms (50.73%), Memory - 42.3 MB (80.12%)

--- my-folder/0424-longest-repeating-character-replacement/solution.java
+++ my-folder/0424-longest-repeating-character-replacement/solution.java
@@ -1,20 +1,18 @@
 class Solution {
-
     public int characterReplacement(String s, int k) {
-        int[] arr = new int[26];
-        int ans = 0;
+        int res = 0;
         int max = 0;
-        int i = 0;
-        for (int j = 0; j < s.length(); j++) {
-            arr[s.charAt(j) - 'A']++;
-            max = Math.max(max, arr[s.charAt(j) - 'A']);
-            if (j - i + 1 - max > k) {
-                arr[s.charAt(i) - 'A']--;
-                i++;
+        int[] count = new int[26];
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            count[s.charAt(right) - 'A']++;
+            max = Math.max(max, count[s.charAt(right) - 'A']);
+            if (right - left + 1 - max > k) {
+                count[s.charAt(left) - 'A']--;
+                left++;
             }
-            ans = Math.max(ans, j - i + 1);
+            res = Math.max(res, right - left + 1);
         }
-        return ans;
+        return res;
     }
 }
-
