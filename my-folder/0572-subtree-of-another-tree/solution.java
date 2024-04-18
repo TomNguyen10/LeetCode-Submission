a2551fb4ea9d2538eb89ba3f3230df249571cf4c
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-18 20:25:30 +0000

    [LeetCode Sync] Runtime - 2 ms (96.48%), Memory - 44.6 MB (16.41%)

--- my-folder/0572-subtree-of-another-tree/solution.java
+++ my-folder/0572-subtree-of-another-tree/solution.java
@@ -18,25 +18,22 @@ class Solution {
         if (isSame(root, subRoot)) {
             return true;
         }
-        if (root == null) {
+        if (root == null ) {
             return false;
         }
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 
-    private boolean isSame(TreeNode p, TreeNode q) {
+    boolean isSame(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
             return true;
         }
-
         if (p == null || q == null) {
             return false;
         }
-
         if (p.val != q.val) {
             return false;
         }
-
         return isSame(p.left, q.left) && isSame(p.right, q.right);
     }
 }
