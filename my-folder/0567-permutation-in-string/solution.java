3ac339063e9ef33da7ae7d7c9b6db5a4c50071ea
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-18 00:33:48 +0000

    [LeetCode Sync] Runtime - 45 ms (24.75%), Memory - 44.7 MB (25.72%)

--- my-folder/0567-permutation-in-string/solution.java
+++ my-folder/0567-permutation-in-string/solution.java
@@ -1,26 +1,36 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) return false;
 
-        int len = s1.length();
-        int left = 0;
-        int right = left + s1.length() - 1;
-
-        char[] arr1 = s1.toCharArray();
-        Arrays.sort(arr1);
-        String val1 = String.valueOf(arr1);
-
-        while (right < s2.length()) {
-            String sub = s2.substring(left, right+1);
-            char[] arr2 = sub.toCharArray();
-            Arrays.sort(arr2);
-            String val2 = String.valueOf(arr2);
-
-            if (val1.equals(val2)) return true;
-            left++;
-            right++;
+        if (s1.length() > s2.length()) {
+            return false;
         }
 
+        Map<Character, Integer> m1 = new HashMap<>();
+        Map<Character, Integer> m2 = new HashMap<>();
+
+        for (int i = 0; i < s1.length(); i++) {
+            char c1 = s1.charAt(i);
+            char c2 = s2.charAt(i);
+            m1.put(c1, m1.getOrDefault(c1, 0) + 1);
+            m2.put(c2, m2.getOrDefault(c2, 0) + 1);
+        }
+
+        if (m1.equals(m2)) return true;
+
+        for (int right = s1.length(); right < s2.length(); right++) {
+            int left = right - s1.length();
+            char cRemove = s2.charAt(left);
+            m2.put(cRemove, m2.get(cRemove) - 1);
+            if (m2.get(cRemove) == 0) {
+                m2.remove(cRemove);
+            }
+            char cAdd = s2.charAt(right);
+            m2.put(cAdd, m2.getOrDefault(cAdd, 0) + 1);
+            if (m1.equals(m2)) {
+                return true;
+            }
+        }
         return false;
     }
 }
+
