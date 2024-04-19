183548025b6eabad7b93ae8d0e4fb34464b973ac
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-19 01:52:41 +0000

    [LeetCode Sync] Runtime - 3 ms (84.99%), Memory - 49.8 MB (25.54%)

--- my-folder/0200-number-of-islands/solution.java
+++ my-folder/0200-number-of-islands/solution.java
@@ -1,31 +1,32 @@
 class Solution {
     public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        
+        int rows = grid.length;
+        int cols = grid[0].length;
         int count = 0;
-        int n = grid.length;
-        int m = grid[0].length;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == '1') {
-                    DFS (grid,i,j);
-                    ++count;
+                    count++;
+                    dfs(grid, j, i);
                 }
             }
         }
-
         return count;
     }
 
-    private void DFS (char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1') {
+    void dfs(char[][] grid, int x, int y) {
+        if (x < 0 || x >= grid[0].length || y < 0 || y >= grid.length || grid[y][x] != '1') {
             return;
         }
-
-        grid[i][j] = '0';
-
-        DFS (grid, i+1, j);
-        DFS (grid, i-1, j);
-        DFS (grid, i, j+1);
-        DFS (grid, i, j-1);
+        grid[y][x] = '0';
+        dfs(grid, x+1, y);
+        dfs(grid, x-1, y);
+        dfs(grid, x, y+1);
+        dfs(grid, x, y-1);
     }
 }
