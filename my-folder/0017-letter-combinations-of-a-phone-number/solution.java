b38a1b044bda55fd78230ceaceb6a4fd5b7a83d3
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-19 14:37:49 +0000

    [LeetCode Sync] Runtime - 5 ms (37.11%), Memory - 42.2 MB (35.15%)

--- my-folder/0017-letter-combinations-of-a-phone-number/solution.java
+++ my-folder/0017-letter-combinations-of-a-phone-number/solution.java
@@ -1,31 +1,32 @@
 class Solution {
-  public List<String> letterCombinations(String digits) {
-    List<String> res = new ArrayList<>();
-    if (digits.isEmpty()) return res;
-    HashMap<Character, String> map = new HashMap<Character, String> ();
-    map.put('2', "abc");
-    map.put('3', "def");
-    map.put('4', "ghi");
-    map.put('5', "jkl");
-    map.put('6', "mno");
-    map.put('7', "pqrs");
-    map.put('8', "tuv");
-    map.put('9', "wxyz");
 
-    backtrack(res, "", digits, 0, map);
-    return res;
-  }
+    List<String> res = new ArrayList<>(); 
+    Map<Character, String> map = new HashMap<>();
 
-  private void backtrack(List<String> res, String cur, String digits, int index, Map<Character, String> map) {
-    if (index == digits.length()) {
-      res.add(cur);
-      return;
+    public List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) {
+            return res;
+        }
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+        backtrack("", 0, digits);
+        return res;
     }
-    char currentDigit = digits.charAt(index);
-    String letter = map.get(currentDigit);
 
-    for (int i = 0; i < letter.length(); i++) {
-      backtrack(res, cur + letter.charAt(i), digits, index+1, map);
+    private void backtrack(String current, int index, String digits) {
+        if (index == digits.length()) {
+            res.add(current);
+            return;
+        }
+        String letters = map.get(digits.charAt(index));
+        for (int i = 0; i < letters.length(); i++) {
+            backtrack(current + letters.charAt(i), index+1, digits);
+        }
     }
-  }
 }
