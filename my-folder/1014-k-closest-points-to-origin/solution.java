a5e14a78c29d2b2e2a3fb5030f6bcd863043fcc2
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-19 15:10:25 +0000

    [LeetCode Sync] Runtime - 31 ms (55.77%), Memory - 47.6 MB (97.56%)

--- my-folder/1014-k-closest-points-to-origin/solution.java
+++ my-folder/1014-k-closest-points-to-origin/solution.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-
         PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
             @Override
             public int compare(int[] p1, int[] p2) {
@@ -9,17 +8,13 @@ class Solution {
                 return Double.compare(distance1, distance2);
             }
         });
-        
         for (int[] point : points) {
             pq.offer(point);
         }
-
-        int[][] result = new int[k][2];
-        
+        int[][] res = new int[k][2];
         for (int i = 0; i < k; i++) {
-            result[i] = pq.poll();
+            res[i] = pq.poll();
         }
-
-        return result;
+        return res;
     }
 }
