ac86ae618b2989bc50e9f61917b855eaf189b6d3
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-19 20:39:08 +0000

    [LeetCode Sync] Runtime - 9 ms (55.01%), Memory - 46.6 MB (32.16%)

--- my-folder/0056-merge-intervals/solution.java
+++ my-folder/0056-merge-intervals/solution.java
@@ -1,22 +1,22 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals == null || intervals.length == 0) {
-            return new int[0][];
-        }
         Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
-        List<int[]> res = new ArrayList<>();
-        res.add(intervals[0]);
+        List<int[]> list = new ArrayList<>();
+        list.add(intervals[0]);
         for (int i = 1; i < intervals.length; i++) {
-            int[] prev = res.get(res.size() - 1);
-            int[] curr = intervals[i];
-            if (prev[1] >= curr[0]) {
-                prev[1] = Math.max(prev[1], curr[1]);
+            int[] last = list.get(list.size() - 1);
+            int[] interval = intervals[i];
+            if (last[1] >= interval[0]) {
+                last[1] = Math.max(last[1], interval[1]);
             }
             else {
-                res.add(curr);
+                list.add(interval);
             }
         }
-
-        return res.toArray(new int[res.size()][]);
+        int[][] res = new int[list.size()][2];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
     }
 }
