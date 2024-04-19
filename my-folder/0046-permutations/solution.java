61f246699165df970e50c72e09a79762e2f2ee92
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-19 01:56:46 +0000

    [LeetCode Sync] Runtime - 2 ms (43.25%), Memory - 44.4 MB (38.90%)

--- my-folder/0046-permutations/solution.java
+++ my-folder/0046-permutations/solution.java
@@ -1,21 +1,25 @@
 class Solution {
+
+    List<List<Integer>> res = new ArrayList<>();
+ 
     public List<List<Integer>> permute(int[] nums) {
-    List<List<Integer>> list = new ArrayList<>();
-    backtrack(list, new ArrayList<>(), nums);
-    return list;
+        backtrack(nums, new ArrayList<>());
+        return res;
     }
 
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
-        if(tempList.size() == nums.length)
-            list.add(new ArrayList<>(tempList));
-        else{
-            for(int i = 0; i < nums.length; i++){ 
-                if(tempList.contains(nums[i])) 
-                    continue; 
-                tempList.add(nums[i]);
-                backtrack(list, tempList, nums);
-                tempList.remove(tempList.size() - 1);
-            }
+    private void backtrack(int[] nums, List<Integer> temp) {
+        if (temp.size() == nums.length) {
+            res.add(new ArrayList<>(temp));
+            return;
         }
-    } 
+        for (int i = 0; i < nums.length; i++) {
+            if(temp.contains(nums[i])) {
+                continue;
+            }   
+            temp.add(nums[i]);
+            backtrack(nums, temp);
+            temp.remove(temp.size() - 1);
+        }
+    }
+
 }
