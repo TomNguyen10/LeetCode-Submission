ec3fd4918b20cc078c7f9398ff84510302e88215
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-20 20:19:23 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 45.9 MB (66.49%)

--- my-folder/0283-move-zeroes/solution.java
+++ my-folder/0283-move-zeroes/solution.java
@@ -1,15 +1,13 @@
 class Solution {
-     public void moveZeroes(int[] nums) {
-        int snowBallSize = 0; 
-        for (int i=0;i<nums.length;i++){
-	        if (nums[i]==0){
-                snowBallSize++; 
+    public void moveZeroes(int[] nums) {
+        int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[idx++] = nums[i];
             }
-            else if (snowBallSize > 0) {
-	            int t = nums[i];
-	            nums[i]=0;
-	            nums[i-snowBallSize]=t;
-            }
+        }
+        for (int i = idx; i < nums.length; i++) {
+            nums[i] = 0;
         }
     }
 }
