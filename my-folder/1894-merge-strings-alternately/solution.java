678dec579d33bfdcaa814c38b54ff7a0286978cf
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-20 19:03:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.4 MB (77.64%)

--- my-folder/1894-merge-strings-alternately/solution.java
+++ my-folder/1894-merge-strings-alternately/solution.java
@@ -1,16 +1,17 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        StringBuilder result = new StringBuilder();
-        int i = 0;
-        while (i < word1.length() || i < word2.length()) {
-            if (i < word1.length()) {
-                result.append(word1.charAt(i));
-            }
-            if (i < word2.length()) {
-                result.append(word2.charAt(i));
-            }
-            i++;
+        StringBuilder sb = new StringBuilder();
+        int min = Math.min(word1.length(), word2.length());
+        for (int i = 0; i < min; i++) {
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i));
         }
-        return result.toString();
+        if (min < word1.length()) {
+            sb.append(word1.substring(min, word1.length()));
+        }
+        else if (min < word2.length()){
+            sb.append(word2.substring(min, word2.length()));
+        }
+        return sb.toString();
     }
 }
