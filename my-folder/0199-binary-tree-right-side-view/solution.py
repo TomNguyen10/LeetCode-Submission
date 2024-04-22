3c17cd32d1e5be0ef300007821aa0cfe978bd8ec
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-22 21:16:11 +0000

    [LeetCode Sync] Runtime - 32 ms (81.57%), Memory - 16.5 MB (83.41%)

--- my-folder/0199-binary-tree-right-side-view/solution.py
+++ my-folder/0199-binary-tree-right-side-view/solution.py
@@ -7,22 +7,23 @@
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
 
-        if not root: return []
-
-        def bfs(root, arr):
-            queue = [root]
-
-            while queue:
-                size = len(queue)
-                for i in range(size):
-                    curr = queue.pop(0)
-                    if i == 0:
-                        arr.append(curr.val)
-                    if curr.right: 
-                        queue.append(curr.right)
-                    if curr.left:
-                        queue.append(curr.left)
-            
         res = []
-        bfs(root, res)
+
+        if not root: return res
+
+        q = [root]
+
+        while q:
+
+            size = len(q)
+
+            for i in range(size):
+                node = q.pop(0)
+                if i == 0:
+                    res.append(node.val)
+                if node.right:
+                    q.append(node.right)
+                if node.left:
+                    q.append(node.left)
+    
         return res
