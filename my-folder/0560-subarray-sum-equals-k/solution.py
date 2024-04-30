b3a12e75b8083e8cbc447e41e22e16083cf3bf38
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-04-30 00:13:25 +0000

    [LeetCode Sync] Runtime - 229 ms (53.37%), Memory - 20.8 MB (8.34%)

--- my-folder/0560-subarray-sum-equals-k/solution.py
+++ my-folder/0560-subarray-sum-equals-k/solution.py
@@ -1,13 +1,11 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         dic = defaultdict(int)
-        count = 0
-        cum_sum = 0
         dic[0] = 1
-
+        curr = 0
+        count = 0
         for num in nums:
-            cum_sum += num
-            count += dic[cum_sum - k]
-            dic[cum_sum] += 1
-        
+            curr += num
+            count += dic[curr - k]
+            dic[curr] += 1
         return count
