9cecbadef11b2568bbbb95c2dae0b91f9e118ca0
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-05-29 02:47:36 +0000

    [LeetCode Sync] Runtime - 39 ms (35.33%), Memory - 16.7 MB (93.75%)

---
+++ my-folder/0404-sum-of-left-leaves/solution.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        def dfs(node):
+            if not node:
+                return
+            if node.left and not node.left.left and not node.left.right:
+                self.res += node.left.val
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return self.res
