f603444905a59417a8381647be12f7447ed4606e
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-06-02 22:26:27 +0000

    [LeetCode Sync] Runtime - 107 ms (97.21%), Memory - 17.7 MB (99.26%)

--- my-folder/0130-surrounded-regions/solution.py
+++ my-folder/0130-surrounded-regions/solution.py
@@ -3,37 +3,33 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        if not board or not board[0]:
-            return
-        
-        def dfs(board, x, y):
-            if x < 0 or x >= len(board[0]) or y < 0 or y >= len(board) or board[y][x] != 'O':
+        def dfs(board, row, col):
+            if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]) or board[row][col] != 'O':
                 return
-            board[y][x] = '#'
-            dfs(board, x + 1, y)
-            dfs(board, x - 1, y)
-            dfs(board, x, y - 1)
-            dfs(board, x, y + 1)
-        
+            board[row][col] = '#'
+            dfs(board, row - 1, col)
+            dfs(board, row + 1, col)
+            dfs(board, row, col - 1)
+            dfs(board, row, col + 1)
+
         rows = len(board)
         cols = len(board[0])
-        
+
         for r in range(rows):
             if board[r][0] == 'O':
-                dfs(board, 0, r)
+                dfs(board, r, 0)
             if board[r][cols - 1] == 'O':
-                dfs(board, cols - 1, r)
+                dfs(board, r, cols - 1)
         
         for c in range(cols):
             if board[0][c] == 'O':
-                dfs(board, c, 0)
+                dfs(board, 0, c)
             if board[rows - 1][c] == 'O':
-                dfs(board, c, rows - 1)
-        
+                dfs(board, rows - 1, c)
+                
         for r in range(rows):
             for c in range(cols):
                 if board[r][c] == 'O':
                     board[r][c] = 'X'
                 elif board[r][c] == '#':
                     board[r][c] = 'O'
-
