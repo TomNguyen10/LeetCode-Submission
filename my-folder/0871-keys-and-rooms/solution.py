8f1633906db65cade00cd21fd53aafa064f8b8bf
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-06-03 22:26:22 +0000

    [LeetCode Sync] Runtime - 58 ms (91.49%), Memory - 17 MB (87.38%)

--- my-folder/0871-keys-and-rooms/solution.py
+++ my-folder/0871-keys-and-rooms/solution.py
@@ -2,13 +2,15 @@ class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         visited = [False] * len(rooms)
         visited[0] = True
-        def dfs(rooms, room, visited):
-            for key in rooms[room]:
+        def dfs(rooms, current, visited):
+            for key in rooms[current]:
                 if not visited[key]:
                     visited[key] = True
                     dfs(rooms, key, visited)
+        
         dfs(rooms, 0, visited)
+
         for visit in visited:
-            if not visit:
-                return False
+            if not visit: return False
+        
         return True
