124f342c7caa4323ffcb595bcd3b0adb037ce6ef
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-06-05 00:19:35 +0000

    [LeetCode Sync] Runtime - 53 ms (42.95%), Memory - 16.7 MB (15.66%)

--- my-folder/1044-find-common-characters/solution.py
+++ my-folder/1044-find-common-characters/solution.py
@@ -1,13 +1,14 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        dic = Counter(words[0])
-        
-        for word in words[1:]:
-            dic &= Counter(word)
+        c = Counter(words[0])
+        for i in range(1, len(words)):
+            word = words[i]
+            c &= Counter(word)
         
         res = []
-        
-        for key, val in dic.items():
+
+        for key, val in c.items():
             res.extend([key] * val)
-            
+        
         return res
+
