d3f452f30bf375394dc90ac12079dae7ceb098c7
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-06-09 18:52:42 +0000

    [LeetCode Sync] Runtime - 129 ms (67.59%), Memory - 36.9 MB (6.55%)

--- my-folder/0648-replace-words/solution.py
+++ my-folder/0648-replace-words/solution.py
@@ -20,25 +20,23 @@ class Trie:
 
     def shortest_path(self, word):
         current = self.root
-        for i in range(len(word)):
-            c = word[i]
+        res = ""
+        for c in word:
             if not current.children[ord(c) - ord('a')]:
                 return word
             current = current.children[ord(c) - ord('a')]
+            res += c
             if current.isEnd:
-                return word[:i+1]
-        
+                return res
         return word
 
 
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        arr = sentence.split(" ")
         root = Trie()
         for word in dictionary:
             root.insert(word)
-        
-        for word in range(len(arr)):
-            arr[word] = root.shortest_path(arr[word])
-        
-        return " ".join(arr)
+        arr = sentence.split(" ")
+        for i in range(len(arr)):
+            arr[i] = root.shortest_path(arr[i])
+        return " ".join(arr) 
