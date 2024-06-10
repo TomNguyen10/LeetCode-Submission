24ae7d0af74e28558bbcf939c63eb48bf4314503
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-06-10 03:41:07 +0000

    [LeetCode Sync] Runtime - 40 ms (52.94%), Memory - 16.5 MB (94.53%)

--- my-folder/0088-merge-sorted-array/solution.py
+++ my-folder/0088-merge-sorted-array/solution.py
@@ -3,9 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        p1, p2 = m - 1, n - 1
         k = m + n - 1
-
+        p1 = m - 1
+        p2 = n - 1
         while p2 >= 0:
             if p1 >= 0 and nums1[p1] > nums2[p2]:
                 nums1[k] = nums1[p1]
@@ -14,4 +14,3 @@ class Solution:
                 nums1[k] = nums2[p2]
                 p2 -= 1
             k -= 1
-        
