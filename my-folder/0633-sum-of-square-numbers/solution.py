9618ed8942271a0afbaac8ce4d227e19f9be4cde
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-06-17 18:30:52 +0000

    [LeetCode Sync] Runtime - 104 ms (58.26%), Memory - 16.4 MB (62.36%)

--- my-folder/0633-sum-of-square-numbers/solution.py
+++ my-folder/0633-sum-of-square-numbers/solution.py
@@ -1,14 +1,15 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        left, right = 0, int (sqrt(c))
-        while left <= right:
-            curr = left**2 + right**2
-            print(curr)
-            if curr == c:
-                return True
-            elif curr < c:
-                left += 1
-            else:
-                right -= 1
-        return False
+        left = 0
+        right = int(sqrt(c))
 
+        while left <= right:
+            num = left ** 2 + right ** 2
+            if num == c:
+                return True
+            elif num > c:
+                right -= 1
+            else:
+                left += 1
+        
+        return False
