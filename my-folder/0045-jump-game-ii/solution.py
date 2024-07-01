13200a57dd0f7f59906fd5f94cb493309520890a
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-07-01 21:49:12 +0000

    [LeetCode Sync] Runtime - 95 ms (95.07%), Memory - 17.4 MB (85.22%)

--- my-folder/0045-jump-game-ii/solution.py
+++ my-folder/0045-jump-game-ii/solution.py
@@ -1,10 +1,12 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        jumps = curEnd = curFar = 0
-        for i in range(len(nums)-1):
+        count = curEnd = curFar = 0
+
+        for i in range(len(nums) - 1):
             curFar = max(curFar, i + nums[i])
 
             if i == curEnd:
                 curEnd = curFar
-                jumps += 1
-        return jumps
+                count += 1
+        
+        return count
