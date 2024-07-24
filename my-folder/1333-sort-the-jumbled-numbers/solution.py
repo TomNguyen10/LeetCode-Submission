6926ff5a075aea46c7ed4fac0e28866c66df0780
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-07-24 03:52:21 +0000

    [LeetCode Sync] Runtime - 956 ms (72.21%), Memory - 25.3 MB (30.35%)

--- my-folder/1333-sort-the-jumbled-numbers/solution.py
+++ my-folder/1333-sort-the-jumbled-numbers/solution.py
@@ -1,24 +1,20 @@
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        def convert(num, mapping):
+        def convert(num):
             if num == 0:
                 return mapping[0]
-            q = deque()
+            q = deque([])
             while num > 0:
                 mod = num % 10
                 q.appendleft(mapping[mod])
                 num //= 10
-
             res = 0
             for digit in q:
                 res = res * 10 + digit
             return res
         
-        # Create a list of tuples where each tuple is (original number, mapped value)
-        mapped_nums = [(num, convert(num, mapping)) for num in nums]
+        dic = [(num, convert(num)) for num in nums]
         
-        # Sort based on the mapped values while maintaining relative order for equal mapped values
-        mapped_nums.sort(key=lambda x: x[1])
-        
-        # Extract and return the original numbers in the sorted order
-        return [num for num, _ in mapped_nums]
+        sorted_dic = sorted(dic, key=lambda x: x[1])
+
+        return [num for num, _ in sorted_dic]
