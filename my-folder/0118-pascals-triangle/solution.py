317b429d94efce2c301bf9f59e45d1bcd1fbcdf2
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-08-23 14:57:27 +0000

    [LeetCode Sync] Runtime - 39 ms (39.77%), Memory - 16.5 MB (31.71%)

--- my-folder/0118-pascals-triangle/solution.py
+++ my-folder/0118-pascals-triangle/solution.py
@@ -1,16 +1,14 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         res = []
-
         res.append([1])
-
         for i in range(1, numRows):
-            prev_row = res[-1]
-            new_row = [1]
-            for j in range(1, i):
-                new_row.append(prev_row[j - 1] + prev_row[j])
-            new_row.append(1)
-            res.append(new_row)
+            prev = res[-1]
+            new = [1]
 
+            for j in range(1, i):
+                add = prev[j-1] + prev[j]
+                new.append(add)
+            new.append(1)
+            res.append(new)
         return res
-        
