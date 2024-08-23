5daf1e0e39099587743c16e404d71ab43ed29ff6
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-08-23 20:29:25 +0000

    [LeetCode Sync] Runtime - 39 ms (22.00%), Memory - 16.4 MB (62.80%)

--- my-folder/1236-n-th-tribonacci-number/solution.py
+++ my-folder/1236-n-th-tribonacci-number/solution.py
@@ -1,9 +1,11 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        arr = [0] * 38
-        arr[1] = 1
-        arr[2] = 1
-        for i in range(len(arr)-3):
-            arr[i+3] = arr[i] + arr[i+1] + arr[i+2]
+        if n <= 1: return n
+        if n == 2: return 1
+
+        a, b, c = 0, 1, 1
+
+        for i in range(2, n):
+            a, b, c = b, c, a + b + c
         
-        return arr[n]
+        return c
