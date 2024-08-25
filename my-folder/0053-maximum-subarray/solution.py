c1e61eb9dc5fe89ffc9ed1c8ecd207d3f1ae6b13
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-08-25 19:53:41 +0000

    [LeetCode Sync] Runtime - 515 ms (72.86%), Memory - 31.1 MB (13.96%)

--- my-folder/0053-maximum-subarray/solution.py
+++ my-folder/0053-maximum-subarray/solution.py
@@ -1,5 +1,7 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        # We will keep track of the current sum
+        # If the current sum < 0, set it to 0
         res = nums[0]
         curr = 0
         for i in range(len(nums)):
