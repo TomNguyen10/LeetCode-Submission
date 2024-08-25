97691945e95e29ed0c5686ae4521041202de3210
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-08-25 19:49:28 +0000

    [LeetCode Sync] Runtime - 101 ms (78.41%), Memory - 17.6 MB (42.41%)

--- my-folder/0045-jump-game-ii/solution.py
+++ my-folder/0045-jump-game-ii/solution.py
@@ -1,12 +1,19 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        count = curEnd = curFar = 0
+        # The main idea is based on greedy. 
+        # Let's say the range of the current jump is [curBegin, curEnd], curFarthest is the farthest point that all points in [curBegin, curEnd] can reach. 
+        # Once the current point reaches curEnd, then trigger another jump, and set the new curEnd with curFarthest, then keep the above steps, as the following:
+
+        res = 0
+        curEnd = 0
+        curFar = 0
 
         for i in range(len(nums) - 1):
             curFar = max(curFar, i + nums[i])
-
             if i == curEnd:
+                res += 1
                 curEnd = curFar
-                count += 1
+
+        return res
         
-        return count
+
