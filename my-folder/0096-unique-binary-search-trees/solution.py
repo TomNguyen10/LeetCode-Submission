bf468cb187dcab2355831314f18be45e750962a8
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-08-26 22:12:44 +0000

    [LeetCode Sync] Runtime - 22 ms (99.29%), Memory - 16.5 MB (65.85%)

--- my-folder/0096-unique-binary-search-trees/solution.py
+++ my-folder/0096-unique-binary-search-trees/solution.py
@@ -1,11 +1,14 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        arr = [0] * (n+1)
-        arr[1] = arr[0] = 1
+        dp = [0] * (n+1)
+        dp[0] = dp[1] = 1
+
         for i in range(2, n+1):
-            for j in range(1, i+1):
+            for j in range(1, i + 1):
                 left = j - 1
                 right = i - j
-                arr[i] += arr[left] * arr[right]
+                dp[i] += dp[left] * dp[right]
+        
+        return dp[n]
 
-        return arr[n] 
+
