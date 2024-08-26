66c96ae3eb0d3547d190d167fccec8f6f7afc7e1
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-08-26 19:34:45 +0000

    [LeetCode Sync] Runtime - 36 ms (45.56%), Memory - 16.4 MB (61.96%)

--- my-folder/0070-climbing-stairs/solution.py
+++ my-folder/0070-climbing-stairs/solution.py
@@ -1,9 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 1:
+        if n <= 2:
             return n
-        one = 1
-        two = 2
-        for i in range(2, n):
-            one, two = two, one + two
-        return two
+        
+        ones = 1
+        twos = 2
+
+        while n > 2:
+            ones, twos = twos, ones + twos
+            n -= 1
+        
+        return twos
