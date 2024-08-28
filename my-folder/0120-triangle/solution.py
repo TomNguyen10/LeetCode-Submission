af882a68b086456b45d8358b4bf201079c716f9e
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-08-28 20:58:15 +0000

    [LeetCode Sync] Runtime - 55 ms (88.32%), Memory - 17.5 MB (97.97%)

--- my-folder/0120-triangle/solution.py
+++ my-folder/0120-triangle/solution.py
@@ -2,12 +2,15 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         for i in range(1, len(triangle)):
             curr = triangle[i]
-            prev = triangle[i - 1]
+            prev = triangle[i-1]
+            
             for j in range(len(curr)):
                 if j == 0:
                     curr[j] += prev[0]
                 elif j == len(curr) - 1:
                     curr[j] += prev[-1]
                 else:
-                    curr[j] = min(curr[j] + prev[j], curr[j] + prev[j - 1])
+                    curr[j] += min(prev[j], prev[j-1])
+            
         return min(triangle[-1])
+                
