6ec60d110829fad6886925b6c7de904e9a14728f
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-09-03 21:24:32 +0000

    [LeetCode Sync] Runtime - 507 ms (36.43%), Memory - 19.3 MB (28.06%)

--- my-folder/0713-subarray-product-less-than-k/solution.py
+++ my-folder/0713-subarray-product-less-than-k/solution.py
@@ -3,14 +3,12 @@ class Solution:
         if k == 0: return 0
 
         pro = 1
-        res = 0
         left = 0
-
+        res = 0
         for right in range(len(nums)):
             pro *= nums[right]
             while pro >= k and left <= right:
                 pro /= nums[left]
                 left += 1
             res += right - left + 1
-        
         return res
