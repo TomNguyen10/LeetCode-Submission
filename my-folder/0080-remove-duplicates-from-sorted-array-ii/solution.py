ccc1dfc979817f83742bbdf7ee8cc9508168b26a
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-09-05 20:55:17 +0000

    [LeetCode Sync] Runtime - 55 ms (34.77%), Memory - 16.4 MB (83.53%)

--- my-folder/0080-remove-duplicates-from-sorted-array-ii/solution.py
+++ my-folder/0080-remove-duplicates-from-sorted-array-ii/solution.py
@@ -1,10 +1,18 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        res = 0
+        '''
+        The basic idea is of two pointers. The fast pointer will traverse the array sequentially one element at a time.
+The other slower pointer is i which does not increment when there is a duplicate, but otherwise increments one position at a time.
+Because this is sorted array, duplicates will be always adjacent.
+
+To remove dupes, all we have to do is to overwrite slow pointer value with fast pointer.
+At the end, point up to which slow pointer has moved, is the unique array, rest of array will be unwanted dupes.
+        '''
+        i = 0
 
         for n in nums:
-            if res < 2 or n > nums[res-2]:
-                nums[res] = n
-                res += 1
+            if i < 2 or n > nums[i - 2]:
+                nums[i] = n
+                i += 1
         
-        return res
+        return i
