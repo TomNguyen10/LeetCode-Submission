c861d657cfd0a3be1f5f3436171a0b046c117897
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-09-06 20:27:13 +0000

    [LeetCode Sync] Runtime - 33 ms (81.96%), Memory - 16.6 MB (23.36%)

--- my-folder/0075-sort-colors/solution.py
+++ my-folder/0075-sort-colors/solution.py
@@ -3,8 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        '''
+        We need 2 pointers to keep track of the 0 and 2, the 1 will automatically in place.
+        Loop through the array, if we find 0 swap it with the left pointer, else if we find 2, swap with the right pointer
+        '''
+
         p1, p2 = 0, len(nums) - 1
         p = 0
+
         while p <= p2:
             if nums[p] < 1:
                 nums[p], nums[p1] = nums[p1], nums[p]
