28a4a110dbf75d59fb8ee152953ebab69aa15798
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-09-08 20:42:01 +0000

    [LeetCode Sync] Runtime - 322 ms (88.80%), Memory - 21.4 MB (93.10%)

--- my-folder/0943-sum-of-subarray-minimums/solution.py
+++ my-folder/0943-sum-of-subarray-minimums/solution.py
@@ -1,20 +1,25 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        MOD = 10**9 + 7
+        '''
+        For each number in the array, we gonna consider all the subarray to the left and count how many it will be the mean
+        If we find a previous less or equal value on its left A[j] <= A[i] then we know that result[i] = result[j] + A[i] * (i-j). Therefore we need an array to keep track of all previous result.
+        We need a stack to track all the previous smaller number of A[i] to its left
+        Loop through the array, while the top of the stack is larger than A[i], pop it out, till we meet number at j index
+        Apply the formula res[i] = res[j] + (i-j) * A[i]
+        Append i to the stack
+        Return the sum of the res array
+        '''
         stack = []
-        result = 0
+        res = [0] * len(arr)
+        MOD = 10**9 + 7
 
-        for i, num in enumerate(arr):
+        for i in range(len(arr)):
+            num = arr[i]
             while stack and arr[stack[-1]] > num:
-                j = stack.pop()
-                k = stack[-1] if stack else -1
-                result = (result + arr[j] * (i - j) * (j - k)) % MOD
-
+                stack.pop()
+            j = stack[-1] if stack else -1 
+            numJ = res[j] if j != -1 else 0
+            res[i] = res[j] + (i - j) * num
             stack.append(i)
 
-        while stack:
-            j = stack.pop()
-            k = stack[-1] if stack else -1
-            result = (result + arr[j] * (len(arr) - j) * (j - k)) % MOD
-
-        return result
+        return sum(res) % MOD
