e25d015a2a0a7295d94bfa6ad8c47d6e23cebe78
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-09-08 21:12:37 +0000

    [LeetCode Sync] Runtime - 1003 ms (56.95%), Memory - 26.9 MB (47.07%)

--- my-folder/2155-find-missing-observations/solution.py
+++ my-folder/2155-find-missing-observations/solution.py
@@ -1,16 +1,18 @@
 class Solution:
     def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
-        m = len(rolls)
-        sumOfN = mean * (m + n) - sum(rolls)
+        total = mean * (len(rolls) + n)
+        nSum = total - sum(rolls)
 
-        if 6*n < sumOfN or sumOfN < n:
+        if nSum > 6 * n or nSum < 1 * n:
             return []
 
-        avg = sumOfN // n
-        remain = sumOfN % n
+        avg = nSum // n
+        remain = nSum % n
         res = [avg] * n
 
         for i in range(remain):
             res[i] += 1
         
         return res
+
+        
